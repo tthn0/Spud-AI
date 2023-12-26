7e0187e2456c6cb48e985fd526d60baa0b91b99f
Khanh <knh.nguyen2002@gmail.com>
2023-12-26 16:03:59 -0600

    test

--- website/app.js
+++ website/app.js
@@ -1,37 +1,42 @@
-const express = require('express');
+//update
+
+const express = require("express");
 const app = express();
 const port = 8000;
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
-const creds = require('./creds.json');
+const creds = require("./creds.json");
 const pool = mysql.createPool(creds);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true })); // Middleware for parsing form data
 
-app.post('/post', async (req, res) => {
-    const body = req.body;
-    const [rows, fields] = await pool.execute('INSERT INTO members (psid, email, password, first, last, discord) VALUES (?, ?, ?, ?, ?, ?)', [
-        body.psid, body.email, body.password, body.first, body.last, body.discord
-    ]);
-    res.json({"Status": "Success"});
+app.post("/post", async (req, res) => {
+  const body = req.body;
+  const [rows, fields] = await pool.execute(
+    "INSERT INTO members (psid, email, password, first, last, discord) VALUES (?, ?, ?, ?, ?, ?)",
+    [body.psid, body.email, body.password, body.first, body.last, body.discord]
+  );
+  res.json({ Status: "Success" });
 });
 
-app.get('/', async (req, res) => {
-    const id = req.query.id;
-    let test;
+app.get("/", async (req, res) => {
+  const id = req.query.id;
+  let test;
 
-    if (id) {
-        try {
-            const [rows, fields] = await pool.execute('SELECT * FROM members WHERE id = ?', [
-                id
-            ]);
+  if (id) {
+    try {
+      const [rows, fields] = await pool.execute(
+        "SELECT * FROM members WHERE id = ?",
+        [id]
+      );
 
-            console.log(rows);
+      console.log(rows);
 
-            if (rows.length > 0) {
-                test = rows.map(row => {
-                    return `
+      if (rows.length > 0) {
+        test = rows
+          .map((row) => {
+            return `
                         <div>
                             <p><strong>ID</strong>: ${row.id}</p>
                             <p><strong>PSID</strong>: ${row.psid}</p>
@@ -41,16 +46,17 @@ app.get('/', async (req, res) => {
                             <p><strong>Discord</strong>: ${row.discord}</p>
                         </div>
                     `;
-                }).join('');
-            }
-        } catch (err) {
-            return res.status(500).send("Error: " + err.message);
-        }
+          })
+          .join("");
+      }
+    } catch (err) {
+      return res.status(500).send("Error: " + err.message);
     }
+  }
 
-    res.render('index', { test });
+  res.render("index", { test });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
